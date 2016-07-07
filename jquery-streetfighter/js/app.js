0cf225adcb7fcb14f363d9c9b9c55923cd2b4e2b
pree1217 <pthutika@gmail.com>
2016-07-06 22:46:33 -0700

    Double Fighters

--- jquery-streetfighter/js/app.js
+++ jquery-streetfighter/js/app.js
@@ -1,13 +1,65 @@
 $(document).ready(function(){
 
-	$(".headerImage")
-	.mouseover(function(){
-		//alert("mouse over");
-		$(this).attr("src", "images/ryu-throwing-hadouken.png");
-	})
-	.mouseout(function(){
-		//alert("mouse out");
-		$(this).attr("src", "images/ryu-standing-still.png");
-	})
-	
+	/* function declarations */
+
+	function throwHadoukenPose(){
+		$('.hadouken').show().animate(
+		  {'left': '1020px'},
+		  500,
+		  function() {
+		    $(this).hide();
+		    $(this).css('left', '520px');
+		  }
+		);
+		$('.hadouken').finish().show()
+		  .animate(
+		    {'left': '1020px'},
+		    500,
+		    function() {
+		      $(this).hide();
+		      $(this).css('left', '520px');
+		    }
+  		);
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.ryu-still').hide();
+	}
+
+	function returnToReadyPose(){
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+		$('.ryu-still').hide();
+	}
+
+
+	/* Event declarations */
+
+	$('.ryu, .hulk-ryu')
+		.mouseenter(function(){
+			$('.ryu-ready').show();
+			$('.ryu-still').hide();
+		})
+		.mouseleave(function(){
+			$('.ryu-still').show();
+			$('.ryu-ready').hide();
+		})
+		.mousedown(function(){
+			throwHadoukenPose();
+		})
+		.mouseup(function(){
+			returnToReadyPose();
+		});
+
+	$(document)
+		.keypress(function(e) {
+			//alert('pressped key');
+			var keycode = (e.keyCode ? e.keyCode : e.which);
+		    if(keycode == 120) {
+		        throwHadoukenPose();
+		    }
+		})
+		.keyup(function(e) {
+			returnToReadyPose();
+		});
+		
 });
