39f6b656ca015529b67c0962c157a7bfc8f7ecbb
pree1217 <pthutika@gmail.com>
2016-07-09 16:02:24 -0700

    Final

---
+++ shoppinglist/scripts/app.js
@@ -0,0 +1,41 @@
+var count = 0;
+
+function addItemToList() {
+	var item = $("#txtAddItem");
+	var items = [];
+
+	if (item.val() != "Add items here ... ") {
+
+		if(item.val().length > 0){
+
+			$('#itemsListAdd').append('<li class="itemsListAdd"><input type="checkbox" onclick="strikeThroughItem(this)"> '+ item.val() +'<img src="trashcan.jpeg" /></li>');
+
+			$("img").click(function() {
+		    	$(this).closest('li').remove();
+		  	});
+
+			$("#txtAddItem").val('');
+		}
+	}
+}	
+
+function strikeThroughItem(that) {
+	if(that.checked)
+	{
+		$(that).closest('li').css("text-decoration", "line-through");
+	} else {
+		$(that).closest('li').css("text-decoration", "none");
+	}
+}
+
+function clearText(){
+	$("#txtAddItem").val('');
+}
+
+$(document).ready(function() {
+	console.log("document ready...");
+
+});
+
+
+	
