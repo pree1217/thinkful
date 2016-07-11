d0f6229267dfdcb7a91132becff8b5df1a9efc2d
pree1217 <pthutika@gmail.com>
2016-07-11 16:45:01 -0700

    closure

--- apps/scripts/fizzbuzz-app.js
+++ apps/scripts/fizzbuzz-app.js
@@ -1,19 +1,44 @@
 $(document).ready(function() {
 
 	var tblFb = document.getElementById("tblFizzBuzz");
+    var number = prompt();
+    var maxNumber = +number;
+    var maxNum = parseInt(maxNumber);
 	var count = 1;
 
-    while (count < 16){
-    	if (count % 15 == 0) {
-    		document.write("FizzBuzz " + "<br />");
-    	} else if (count % 3 == 0) {
-    		document.write("Fizz " + "<br />");
-    	} else if (count % 5 == 0) {
-    		document.write("Buzz " + "<br />");
-    	} else {
-    		document.write(count + "<br />");
-    	}
-      	count++;
+    var printFizzBuzz = function (max) {
+
+        while (count < max){
+        	if (count % 15 == 0) {
+        		document.write("FizzBuzz " + "<br />");
+        	} else if (count % 3 == 0) {
+        		document.write("Fizz " + "<br />");
+        	} else if (count % 5 == 0) {
+        		document.write("Buzz " + "<br />");
+        	} else {
+        		document.write(count + "<br />");
+        	}
+          	count++;
+        }
     }
+
+    printFizzBuzz(maxNum);
+
+    /*
+    var names = ['Jane', 'Mary', 'John', 'Tony'];
+    var compliment = " is great!";
+
+    //function expression - creating a function and assigning it to a variable
+    //function declaration - creating a function but not assigning it to a variable
+    var doSomething = function doSomething(arr,phrase) {
+        var returnArr = [];
+        for(var i=0; i<arr.length; i++){
+            returnArr.push(arr[i] + phrase);
+        }
+        return returnArr;
+    }
+
+    console.log(doSomething(names,compliment));
+    */
 	
 });
