8d21fc67d5408007afe126b4a8976643ab43b791
pree1217 <pthutika@gmail.com>
2016-07-19 14:56:30 -0700

    load json from local file

--- nutritionquiz/scripts/app.js
+++ nutritionquiz/scripts/app.js
@@ -1,36 +1,5 @@
 /* variable declarations */
-var objQuestions = [
-	    {
-	    	"question":"Only one juice is high in iron. Is it ", 
-	    	"choices": ["orange", "prune","carrot","apricot"],
-	    	"answer": "(b) A cup of prune juice has 3 milligrams of iron (that's 37% of the RDA for men, 17% of the RDA for premenopausal women).",
-	    	"correctAnswer": ["prune"]
-	    }, 
-	    {
-	    	"question":" Vitamin E is one of the few major nutrients not listed on nutrition labels, in part because only a few foods contain significant amounts. What are the best sources? ", 
-	    	"choices": ["eggs", "wheat germ","safflower oil","nuts"],
-	    	"answer": "(b, c, and d) Vegetable oils (except olive oil) and products made from them (such as margarine) are the richest sources. Nuts and wheat germ are also good. But most foods rich in vitamin E are very high in fat. ",
-	    	"correctAnswer": ["wheat germ","safflower oil","nuts"]
-	    }, 
-	    {
-	    	"question":"Sulforaphane, a compound thought to protect against cancer, is found in ", 
-	    	"choices": ["broccoli", "cabbage","tea","kale"],
-	    	"answer": "(a, b, and d) It is found primarily in members of the Brassica family, also known as cruciferous vegetables, such as broccoli, cabbage, kale, and cauliflower. These and other vegetables also contain other protective elements, some of which may not have been identified yet. ",
-	    	"correctAnswer": ["broccoli", "cabbage","kale"]
-	    }, 
-	    {
-	    	"question":"Say calcium and most people think milk, but other foods are rich in calcium as well—such as which of the following? ", 
-	    	"choices": ["dried figs", "broccoli", "dried beans", "almonds"],
-	    	"answer": "(all) Ounce for ounce, dried figs, broccoli, and cooked dry beans have as much or more calcium than milk. Ofcourse, you shouldn't try to get all your calcium from figs and almonds—both are high in calories. ",
-	    	"correctAnswer": ["dried figs", "broccoli", "dried beans", "almonds"]
-	    },
-	    {
-	    	"question": " Despite some rumors, a lime juice marinade cannot really 'cook' raw fish or shellfish and kill all bacteria.",
-	    	"choices": ["true", "false"],
-	    	"answer": "(True) Lime juice may kill bacteria on the surface of fish or shellfish, but it won't kill any dangerous microorganisms below the surface. Eating raw fish or shellfish marinated in lime juice (ceviche) is risky.",
-	    	"correctAnswer": ["true"]
-	    }
-];
+var objQuestions = [];
 var elemNo = 1;
 var numQuestions = 0;
 var html='';
@@ -125,27 +94,21 @@ function loadQuiz() {
 /* document load activity */
 //$(document).ready(function() {
 $(function() {
-	var arrData = [];
 
-	var myData = $.getJSON('scripts/data.json', function(data){
-		console.log(data);
+    //start ajax request
+    $.ajax({
+        url: "scripts/data.json",
+        dataType: "json",
+        success: function(data) {
+            objQuestions = data[0].data;
 
-		$.each(data, function(index,value){
-			console.log("value: " + value);
-			arrData.push(value);
-		});
+			loadQuiz();
 
-		return arrData;
-	});
-
-	objQuestions = myData;
-	console.log("data: " + objQuestions);
-
-	loadQuiz();
-
-	elemNo=1;
-	numQuestions=objQuestions.length;
-	
-	$("#question"+elemNo).show();
-
+			elemNo=1;
+			numQuestions=objQuestions.length;
+			
+			$("#question"+elemNo).show();
+    
+        }
+    });
 });
